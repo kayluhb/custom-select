e618789bfc61cadf5582a51ec340b44a03500260
Caleb Brown <killerdillercaleb@gmail.com>
2012-12-05 12:41:31 -0500

    adding some focus to the check as well as some more finesse to the js

--- README.markdown
+++ README.markdown
@@ -1,1 +1,27 @@
 # Custom Select
+A simple plugin for creating custom select drop downs.
+
+Call the plugin with
+
+    $('jquery-selector').customSelect();
+
+The first class is what the plugin uses to style the select. If the element does not have any class, it will default to 'custom-select'.
+
+There are no parameters for this plugin at this time
+
+## Example
+
+    <select class="custom-select">
+      <option>Option 1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+    </select>
+
+    <script>
+        (function($){
+            function init(){
+                $('.custom-select').customSelect();
+            }
+            $(init);
+        }(jQuery));
+    </script>
--- jquery.custom-select.js
+++ jquery.custom-select.js
@@ -1,5 +1,10 @@
 /*
-Call the plugin with $('jquery-selector').customSelect();
+Call the plugin with
+
+    $('jquery-selector').customSelect();
+
+The first class is what the plugin uses to style the select. If the element does not have any class, it will default to 'custom-select'.
+
 */
 (function($) {
     var CustomSelect = function(el, opts) {
@@ -7,26 +12,43 @@ Call the plugin with $('jquery-selector').customSelect();
         var settings = $.extend({}, $.fn.customSelect.defaults, opts),
             selected = ':selected',
             $el = $(el),
-            $wrap = $('<div/>'),
+            $wrap = $('<span/>'),
+            cls = $el.attr('class') === undefined ? 'custom-select' : $el.attr('class'),
             cur = $el.find(selected),
-            cls = $el.attr('class');
+            firstCls = cls.split(' ')[0],
+            innerCls = firstCls + '-inner',
+            id = $el.attr('id'),
+            f = 'focus';
 
+        // Set id on input if it doesn't have one
+        if (!id || id.length < 1) {
+            id = $el.attr('id', '_custom_select_' + $.fn.customSelect.uid++).attr('id');
+        }
+        $el.addClass(cls);
+        
         $wrap
-            .addClass(cls + '-wrap')
-            .html('<span class="' + cls + '"><span class="inner">' + cur.text() + '</span></span>');
+            .addClass(firstCls + '-wrap')
+            .html('<span class="' + cls + '" id="cs_' + id + '" aria-controls="' + id + '"><span class="' + innerCls + '">' + cur.text() + '</span><i></i></span>');
 
-        var $inner = $wrap.find('.inner');
+        var $inner = $wrap.find('.' + innerCls);
 
         $el
             .after($wrap)
-            .css({ fontSize:$el.next().css('font-size'), opacity:0 })
-            .change(function(){
+            .css({ fontSize: $el.next().css('font-size'), opacity: 0 })
+            .change(function () {
                 $inner.text($(this).find(selected).text());
+            })
+            .focusin(function () {
+                $wrap.addClass(f);
+            })
+            .focusout(function () {
+                $wrap.removeClass(f);
             });
 
         $wrap.append($el);
     };
-    $.fn.customSelect = function(options) {
+
+    $.fn.customSelect = function (options) {
         return this.each(function(idx, el) {
             var $el = $(this), key = 'customSelect';
             // Return early if this element already has a plugin instance
@@ -37,6 +59,10 @@ Call the plugin with $('jquery-selector').customSelect();
             $el.data(key, customSelect);
         });
     };
-    // default settings
+
+    // Static properties
+    $.fn.customSelect.uid = 0;
+
+    // Default settings
     $.fn.customSelect.defaults = { /* nothing yet */ };
 })(jQuery);
